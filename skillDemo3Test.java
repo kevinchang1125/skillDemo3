68307c2ed9554fef91e1e3bf0e56875c35174bde
kevinchang1125 <42626473+kevinchang1125@users.noreply.github.com>
2022-02-03 23:00:07 -0800

    test file + fail

---
+++ skillDemo3Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemo3Test {
+    @Test
+    public void addition() {
+        assertEquals(1, skillDemo3.returnTwo());
+    }
+}
